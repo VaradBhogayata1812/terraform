c3fd37a7b4bb1837157ff86e38b51d5ed54d903f
VaradBhogayata1812 <146085585+VaradBhogayata1812@users.noreply.github.com>
2024-01-19 01:36:00 +0000

    Delete variables.tf

--- variables.tf
+++
@@ -1,6 +0,0 @@
-provider "google" {
-  credentials = file("/home/varadbhogayata78/sapient-mote-402310-7b5ee605541d.json")
-  project     = "sapient-mote-402310"
-  region      = "us-central1"
-  zone        = "us-central1-c"
-}
